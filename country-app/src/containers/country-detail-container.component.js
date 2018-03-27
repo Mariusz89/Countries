ecbd1c44396def20cb6d437160964ecbedab78a3
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2018-03-27 19:16:32 +0200

    first commit

---
+++ country-app/src/containers/country-detail-container.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getCountry } from '../actions/actions-countries';
+import CountryDetails from '../presentational/country-details.component';
+
+class CountryDetailsContainer extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    componentDidMount() {
+        this.props.dispatch(getCountry(this.props.params.id));
+    }
+
+    render() {
+        return (
+            <CountryDetails className='country-details' country={this.props.selectedCountry} />
+        )
+    }
+}
+
+const mapStateToProps = function (store) {
+    return {
+        selectedCountry: store.countriesReducer.selectedCountry
+    };
+};
+
+export default connect(mapStateToProps)(CountryDetailsContainer);
